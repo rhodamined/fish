bcfb3696ca681c2e5d49e2b63e1a264888cfdc58
rhodamined <rhodamined@gmail.com>
2025-04-22 16:58:49 -0400

    gish oyster eel

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express(); // init express app
-const port = process.env.PORT || 3005; 
+const port = process.env.PORT || 3003; 
 
 app.use(express.static('dist'));
 app.use(bodyParser.json());
